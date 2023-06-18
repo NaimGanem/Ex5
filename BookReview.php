7e90d973a48ce909c93193e1ff9ca605fe074885
Naim <naimreem1@gmail.com>
2023-06-18 18:16:49 +0300

    Ex5

--- BookReview.php
+++ BookReview.php
@@ -55,7 +55,7 @@ mysqli_close($connection);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BookPage</title>
 </head>
 <body>
 <script src="incldues/booklistReview.js"></script>
